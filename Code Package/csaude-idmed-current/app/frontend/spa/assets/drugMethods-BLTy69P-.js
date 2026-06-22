2226ac84d23ee40fcd9afc61fe07e0721c01722f
colaconhongo <59443536+colaconhongo@users.noreply.github.com>
2026-06-22 20:38:07 +0200

    Last Release
    Last Release

--- Code Package/csaude-idmed-current/app/frontend/spa/assets/drugMethods-BLTy69P-.js
+++
@@ -1,1 +0,0 @@
-import{b0 as u,bP as t}from"./index-fl5zyxGE.js";const s=u(t);function n(){function r(e){return s.query().withAllRecursive(1).where("id",e).first()}return{getDrugFirstLevelById:r}}export{n as u};
